5bb9e356b384e06fe090446f119b72b395d0c535
aifyzaa <silminurazmie@gmail.com>
2025-04-15 22:41:51 +0700

    update link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,35 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faSpotify, 
+  faDeviantart, 
+  faInstagram, 
+  faWhatsapp, 
+  faXbox, 
+  faSteam, 
+  faDiscord,
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import { faCircleExclamation } from '@fortawesome/free-regular-svg-icons';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
   return (
     <footer>
-      <div>
-        <a href="https://github.com/rezaafidan" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/rezaafidan/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+      <div className="footer-icons">
+        <a href="https://github.com/rezaafidan" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+        <a href="https://www.linkedin.com/in/rezaafidan/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+        <a href="https://open.spotify.com/user/w5h27pldzen825c0l2cm4mldw" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSpotify} /></a>
+        <a href="https://www.deviantart.com/rezaafidan" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDeviantart} /></a>
+        <a href="https://www.instagram.com/rezaafidan/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+        <a href="https://wa.me/6281238900703" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faWhatsapp} /></a>
+        <a href="https://account.xbox.com/en-us/profile?gamertag=RpMEzaR" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faXbox} /></a>
+        <FontAwesomeIcon icon={faCircleExclamation} title="Info" />
+        <a href="https://steamcommunity.com/id/76561199789985658" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSteam} /></a>
+        <a href="https://discordapp.com/users/805926838257057812" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDiscord} /></a>
       </div>
-      <p>A <a href="https://linktr.ee/rezaafidan" target="_blank" rel="noreferrer">Reza Afidan</a>'s' Portfolio Website</p>
+      <p>A <a href="https://linktr.ee/rezaafidan" target="_blank" rel="noreferrer">Reza Afidan</a>'s Portfolio Website</p>
     </footer>
   );
 }
